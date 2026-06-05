9a51f8677442b762be8fd492ae9508959894e3d4
Karen Sittig <kasittig@gmail.com>
2026-06-05 11:50:44 -0400

    Revert to Epley formula for E1RM calculation

--- src/utils/calcE1RM.ts
+++ src/utils/calcE1RM.ts
@@ -1,3 +1,4 @@
 export function calcE1RM(weight: number, reps: number): number {
-  return reps === 1 ? weight : weight * (1 + 0.0333 * reps);
+  // Epley formula
+  return weight * (1 + (reps / 30));
 }
