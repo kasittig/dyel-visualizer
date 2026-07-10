d22f8daa6a44d0ebacb29d62b9f1af5da304df47
Karen Sittig <kasittig@gmail.com>
2026-07-10 11:43:23 -0400

    Vitest args to hopefully not OOM as much

--- packages/app/vitest.config.ts
+++ packages/app/vitest.config.ts
@@ -11,6 +11,12 @@ const coefficientsData = JSON.parse(fs.readFileSync(coefficientsPath, 'utf-8'));
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    pool: 'vmThreads',
+    poolOptions: {
+      vmThreads: {
+        memoryLimit: '500MB', // Accepts fixed values (e.g., '500MB') or percentages (e.g., 0.5)
+      },
+    },
   },
   define: {
     __MODIFIER__EFFECTS__: JSON.stringify(modifierEffectsData),
--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -10,6 +10,12 @@ const coefficients = JSON.parse(fs.readFileSync(coefficientsPath, 'utf-8'));
 export default defineConfig({
   test: {
     environment: 'node',
+    pool: 'vmThreads',
+    poolOptions: {
+      vmThreads: {
+        memoryLimit: '500MB', // Accepts fixed values (e.g., '500MB') or percentages (e.g., 0.5)
+      },
+    },
   },
   define: {
     __MODIFIER__EFFECTS__: JSON.stringify(modifierEffectsData),
