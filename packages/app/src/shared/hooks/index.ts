cf8a22e8a0b57ba5d335e11a1858f34b7ce91c17
Karen Sittig <kasittig@gmail.com>
2026-08-02 21:00:11 -0400

    Address mobile foundation review feedback

--- packages/app/src/shared/hooks/index.ts
+++ packages/app/src/shared/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useCsvResource, type CsvResource } from './useCsvResource';
 export { useLocalStorageState } from './useLocalStorageState';
+export { MOBILE_LAYOUT_QUERY, useMediaQuery } from './useMediaQuery';
 export { useSortableRows, type SortDirection } from './useSortableRows';
